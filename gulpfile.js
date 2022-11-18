f5784c31e4f54691958d43a4f48e6ce0fe1963d6
feruzu <fernanda23demarzo@gmail.com>
2022-11-17 23:47:03 -0300

    Aligerar imágenes con Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
+
+// CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+
+
+// IMG
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
 
 function css(done){
 
@@ -13,6 +21,33 @@ function css(done){
     done(); //Callback que avisa cuando finaliza
 }
 
+function img(done) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+    .pipe(  cache( imagemin (opciones)) )
+    .pipe( dest('build/img') )
+
+
+    done();
+}
+
+function versionWebp(done) {
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+    .pipe( webp(opciones) )
+    .pipe( dest('build/img'))
+
+    done();
+}
+
+
+
 function dev(done) {
     watch('src/scss/**/*.scss', css)
 
@@ -20,4 +55,6 @@ function dev(done) {
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.img = img;
+exports.versionWebp = versionWebp;
+exports.dev = parallel ( img, versionWebp, dev );
