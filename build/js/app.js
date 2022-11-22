9a73a2f501ab5db8cbc38f7da1cd33288a9bde6f
feruzu <fernanda23demarzo@gmail.com>
2022-11-22 13:33:59 -0300

    Galeria

--- build/js/app.js
+++ build/js/app.js
@@ -9,5 +9,43 @@
  function crearGaleria() {
      const galeria = document.querySelector('.galeria-img');
 
-     galeria.textContent = 'Vamos a crear la galeria';
+     for(let i = 1; i <= 12; i++){
+        const imagen = document.createElement('picture');
+        imagen.innerHTML = `
+        <source srcset="build/img/galeria-min/${i}.webp" type="image/webp" />
+        <img src="build/img/galeria-min/${i}.jpg" alt="imagen galeria" />
+        `;
+
+        imagen.onclick = function() {
+            mostrarImagen(i);
+        }
+
+        galeria.appendChild(imagen);
+     }
+ }
+
+
+ function mostrarImagen(id) {
+    const imagen = document.createElement('picture');
+    imagen.innerHTML = `
+    <source srcset="build/img/galeria/${id}.webp" type="image/webp" />
+    <img src="build/img/galeria/${id}.jpg" alt="imagen galeria" />
+    `;
+
+    // overlay img
+    const overlay = document.createElement('DIV');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+    overlay.onclick = function(){
+        const body = document.querySelector('body');
+        body.classList.remove('fijar');
+        overlay.remove();
+    }
+
+
+
+    // añade a html
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
+    body.classList.add('fijar');
  }
